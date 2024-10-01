284535e079c0273dc5f2a4d8fd1dbda367757345
krdavidson <151591489+krdavidson@users.noreply.github.com>
2024-10-01 15:44:23 +1300

    Update index-card.component.ts

--- pokedex/src/app/index-card/index-card.component.ts
+++ pokedex/src/app/index-card/index-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { RouterModule } from '@angular/router';
+import { DetailsComponent } from "../details/details.component";
 
 @Component({
   selector: 'app-index-card',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DetailsComponent],
   templateUrl: './index-card.component.html',
   styleUrl: './index-card.component.css'
 })
